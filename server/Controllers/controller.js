62c6ce17db1df187e4ebad16c02cf803c92586cc
emmy <gmemmy22@gmail.com>
2018-12-01 13:41:06 +0100

    add api api-endpoints for editing a specific red-flag record

--- server/Controllers/controller.js
+++ server/Controllers/controller.js
@@ -13,34 +13,94 @@ app.post("/newRecord", (req, res) => {
 })
 
 
-//This endpoint grabs all red flag records
-app.get("/", (req, res) => {
+/***
+*	@description  GET all red-flag records.
+*	@param {object} req - HTTP Request
+* @param {object} res - HTTP Response
+***/
+
+app.get("/records", (req, res) => {
 	let redFlags =  Incidents.filter(Incident => Incident.type === "Red Flag");
 	if (redFlags.length >= 1) {
-			res.json({
-				status: 200,
-				data: redFlags,
+			res.send({
+				"status": 200,
+				"data": redFlags,
 			});
 	} else {
-			res.json({
-					status: 404,
-					error: "No Red Flag Records Found!",
+			res.send({
+					"status": 404,
+					"error": "No Red Flag Records Found!",
 			});
 	}
 })
 
-//This endpoint grabs a specific red flag record
+/***
+*	@description  GET a specific red flag record.
+*	@param {object} req - HTTP Request
+* @param {object} res - HTTP Response
+***/
+
 app.get("/flagRecord", (req, res) => {
 	const specificRecord = Incidents.find(Incident => Incident.id === Number(req.params.id));
 	if (specificRecord = false) {
- res.json({
-		status: 404,
-		message:	"Oops! No record found with this id",
+ res.send({
+		"status": 404,
+		"error":	"Oops! No record found with this id!",
 	});
 	} else {
-		res.json({
-			status: 200,
-			data:	[specificRecord],
+		res.send({
+			"status": 200,
+			"data":	[specificRecord],
+		});
+	}
+})
+
+
+/***
+*	@description  EDIT a specific red flag record.
+*	@param {object} req - HTTP Request
+* @param {object} res - HTTP Response
+***/
+
+app.put("/comment", (req, res) => {
+	const specificRecord = Incidents.find(Incident => Incident.id === Number(req.params.id));
+	//Edit record's comment
+	if (specificRecord) {
+		const { recordComment } = req.body;
+		if (specificRecord.comment = recordComment) {
+		res.send({
+			"status":	200,
+			"data":	[{
+				"id": specificRecord.id,
+				"message":	"Updated red-flag record's comment",
+			}]
+		})
+	} else {
+		res.send({
+			"status": 404,
+			"message": "Record not found!",
+		});
+	}}
+})
+
+app.put("/location", (req, res) => {
+	const specificRecord = Incidents.find(Incident => Incident.id === Number(req.params.id));
+	//Edit record's location
+	if (specificRecord) {
+		const { recordLocation } = req.body;
+		if(specificRecord.location = recordLocation) {
+			res.send({
+				"status":	200,
+				"data":	[{
+					"id":	specificRecord.id,
+					"message":	"Updated red-flag record’s comment",
+				}]
+			})
+		}
+	} else {
+		res.send({
+			"status":	404,
+			"message":	"Record not found",
 		});
 	}
 })
